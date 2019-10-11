44cee7fcb8a5b1e08cce7d7d2f427eb410c38afd
wendy hsiao <wendy19940331@gmail.com>
2019-10-11 23:46:49 +0800

    add routes and views for register and login pages

--- app.js
+++ app.js
@@ -19,6 +19,30 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
+
+// 認證系統的路由
+// 登入頁面
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+// 登入檢查
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+// 註冊頁面
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+// 註冊檢查
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+// 登出
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
+
 // 設定 express port 3000
 app.listen(port, () => {
   console.log(`App is running on port ${port}!`)
