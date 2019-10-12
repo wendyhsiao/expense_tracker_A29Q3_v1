6c1c93c8357da273afc81fdd59c591003ce7820b
wendy hsiao <wendy19940331@gmail.com>
2019-10-12 14:20:11 +0800

    add login using passport and express-session

--- app.js
+++ app.js
@@ -6,16 +6,34 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const port = 3000
-
+// 設定 models
 const db = require('./models')
 const Record = db.Record
 const User = db.User
+
+const session = require('express-session')
+const passport = require('passport')
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: 'your secret key',
+  resave: 'false',
+  saveUninitialized: 'false',
+}))
+// 使用 Passport - 要在「使用路由器」前面
+app.use(passport.initialize())
+app.use(passport.session())
+require('./config/passport')(passport)
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next()
+})
 
 
 // 使用路由器
---
+++ config/passport.js
@@ -0,0 +1,41 @@
+const LocalStrategy = require('passport-local').Strategy
+
+// 載入 User models
+const db = require('../models')
+const User = db.User
+
+module.exports = passport => {
+  passport.use(
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+      User.findOne({ where: { email: email } })
+        .then(user => {
+          if (!user) {
+            return done(null, false, { message: 'That email is not registered' })
+          }
+
+          if (user.password != password) {
+            console.log('user password not correct.')
+            return done(null, false, { message: 'Email or Password incorrect' })
+          }
+          return done(null, user)
+          // bcrypt.compare(password, user.password, (err, isMatch) => {
+          //   if (err) throw err
+          //   if (isMatch) {
+          //     return done(null, user)
+          //   } else {
+          //     return done(null, false, { message: 'Email or Password incorrect' })
+          //   }
+          // })
+        })
+    })
+  )
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+  passport.deserializeUser((id, done) => {
+    User.findByPk(id).then((user) => {
+      done(err, user)
+    })
+  })
+}
